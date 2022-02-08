4365fcb9619d7dc8438e654281aea46d62d75029
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-02-08 17:17:43 -0500

    added model page for functions

---
+++ model/data-layer.php
@@ -0,0 +1,14 @@
+<?php
+/* week6/model/data-layer.php */
+
+function getDesserts()
+{
+    return array('chocolate'=>'chocolate mousse',
+        'vanilla'=>'vanilla custard',
+        'strawberry'=>'strawberry shortcake');
+}
+
+function getColors()
+{
+    return array('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet');
+}
