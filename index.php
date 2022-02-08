7f45f75d28c8a313c9071fe7b2b7fb9bf23b459c
Conor O'Brien <obrien.conor@student.greenriver.edu>
2022-02-08 16:06:42 -0500

    created week6 router, info view, gitignore

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//this is the controller page for AMD
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require the autoload file
+require_once('vendor/autoload.php');
+
+//create an instance of the Base class for fat free
+$f3 = Base::instance();
+
+$f3->route('GET /', function()
+{
+    $views = new Template();
+    echo $views->render('views/info.html');
+});
+
+//run fat-free -> invokes
+$f3->run();
